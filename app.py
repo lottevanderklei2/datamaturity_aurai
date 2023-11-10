08607446b35fccb2952b6afc4714a7b79c490fcc
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-11-10 11:14:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,42 @@ import plotly.express as px
 
 # Sample data (replace this with your data)
 data = {
-    'Question': ['Q1', 'Q2', 'Q3', 'Q4', 'Q5'],
-    'Answer1': [3, 4, 2, 5, 1],
-    'Answer2': [2, 5, 3, 4, 1],
-    'Answer3': [5, 1, 4, 2, 3],
+    'Question': ['Business & Data Alignment', 
+                 'Data Driven Organisation', 
+                 'Data Governance & Management', 
+                 'Data Architecture & Infrastructure', 
+                 'Cloud', 
+                 'ETL Processes', 
+                 'Machine Learning engineering', 
+                 'Innovation & Experimentation', 
+                 'Data Analytics'],
 }
 
-df = pd.DataFrame(data)
+df_questions = pd.DataFrame(data)
 
 # Streamlit app
-st.title("Radar Chart App")
+st.title("Data maturityscan Aurai")
 
-# Display the questions and answers
-st.write("### Questions and Answers:")
-st.write(df)
+# Initialize answers dictionary
+answers = {}
 
-# Create a radar chart
-fig = px.line_polar(df, r=df.iloc[:, 1:], theta='Question', line_close=True)
+# Loop through each question
+for index, row in df_questions.iterrows():
+    st.write(f"### Question {index + 1}: {row['Question']}")
+    
+    # Use unique key for each question's response input
+    response_key = f"response_{index}"
+    response = st.slider("Select your answer", min_value=1, max_value=5, key=response_key)
+    
+    # Save the response in the answers dictionary
+    answers[row['Question']] = response
+
+# Display the answers
+st.write("### Your Answers:")
+answers_df = pd.DataFrame(list(answers.items()), columns=['Question', 'Answer'])
+st.write(answers_df)
+
+# Create a radar chart based on the answers
+fig = px.line_polar(answers_df, r='Answer', theta='Question', line_close=True)
 st.write("### Radar Chart:")
 st.plotly_chart(fig)
-
